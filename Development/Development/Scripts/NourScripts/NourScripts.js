cf658d7021536471ee17b903eb1a1f6dd74b36e9
Bob Finger <rfinger1337@gmail.com>
2016-09-18 14:50:50 -0400

    add files to nours page

--- Development/Development/Scripts/NourScripts/NourScripts.js
+++ Development/Development/Scripts/NourScripts/NourScripts.js
@@ -1,3 +1,36 @@
 ﻿$(document).ready(function () {
 	$("input").checkboxradio();
+
+	$("#p1Ready").click(function () {
+		PlayerOneWins();
+	});
+
+	$("#p2Ready").click(function () {
+		PLayerTwoWins();
+	});
+
+	$("#reset").click(function () {
+		ResetImages();
+	});
+
 });
+
+
+function ResetImages() {
+	
+	$('#playerOneImage').attr('src', '../Content/Images/bugs_bunny.jpg');
+	$('#playerTwoImage').attr('src', '../Content/Images/bugs_bunny.jpg');
+}
+
+function PLayerTwoWins() {
+
+	$('#playerOneImage').attr('src', '../Content/Images/Lose.jpg');
+	$('#playerTwoImage').attr('src', '../Content/Images/tauntLeft.jpg');
+}
+
+
+function PlayerOneWins() {
+
+	$('#playerOneImage').attr('src', '../Content/Images/tauntRight.jpg');
+	$('#playerTwoImage').attr('src', '../Content/Images/Lose.jpg');
+}
